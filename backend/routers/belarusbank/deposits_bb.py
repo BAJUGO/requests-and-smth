9ac638f3da0bba8d9cccfe483cb3f343e4bb5d25
Dima <texetbuy@gmail.com>
2026-02-02 09:39:31 +0300

    httpx start

--- backend/routers/belarusbank/deposits_bb.py
+++ backend/routers/belarusbank/deposits_bb.py
@@ -11,25 +11,21 @@ bb_link = "https://belarusbank.by/api/deposits_info"
 
 @router.get("/all_deps")
 async def get_all_deposits():
-    response = request("GET", url=bb_link)
+    response = httpx.get(bb_link)
     return response.json()
 
 
 @router.get("/dep_by_id/{dep_id}")
 async def get_dep_by_id(dep_id: int):
-    response = request("GET", url=bb_link)
+    response = httpx.get(bb_link)
     return response.json().get(str(dep_id))
 
 
 @router.get("/dep_by_currency/{currency}")
 async def get_dep_by_currency(currency: str):
-    response = request("GET", url=f"{bb_link}?deposit_currency={currency}")
+    response = httpx.get(url=bb_link, params={"deposit_currency": currency})
+    print(response.headers.get("content-type"))
     return response.json()
 
-
-@router.get("/all_deps_https")
-async def get_all_deposits():
-    response = httpx.get(bb_link)
-    return response.json()
 
 # TODO: Selenium, httpx
