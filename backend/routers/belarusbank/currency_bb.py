f1eaf4fb3de1350e8a218e33db1c622d94a3a977
Dima <texetbuy@gmail.com>
2026-02-02 13:05:26 +0300

    BB exchanges in city

--- backend/routers/belarusbank/currency_bb.py
+++ backend/routers/belarusbank/currency_bb.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 import httpx
+from transliterate import translit, detect_language
 
 
 router = APIRouter(prefix="/currency", tags=["CURRENCY"])
@@ -13,3 +14,32 @@ async def get_all_everything_bb():
     return r.json()
 
 
+@router.get("/all_exchanges_in_city/{city}")
+async def get_all_exchanges_city(city: str):
+    if not detect_language(city):
+        city = translit(city, 'ru')
+    r = httpx.get(bb_cur_link, params={"city": city})
+    return r.json()
+
+
+# The most useless function ever created by people
+@router.get("/exchanges_in_city/{city}/{currencies}", description="In - bank buys, out - bank sells")
+async def determined_exchanges_in_city(city: str, currency: str, in_or_out: str):
+    if not detect_language(city):
+        city = translit(city, 'ru')
+    r = httpx.get(bb_cur_link, params={"city": city})
+    final_banks = []
+    for bank in r.json():
+        final_bank = {
+            "street": bank["street_type"] + bank["street"] + "" + bank["filials_text"],
+            f"{currency.upper()}_{in_or_out}": f"{bank[f"{currency.upper()}_{in_or_out}"]}"
+        }
+        final_banks.append(final_bank)
+    return final_banks
+
+
+
+#TODO: Поработать с хаосом
+@router.get("/chaos")
+async def chaos():
+    return httpx.get('https://belarusbank.by/open-banking/v1.0/banks/AKBBBY2X/services').json()
